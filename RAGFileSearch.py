0ac94e5a8ba5f6376b6e736f641892c2ba22a019
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-22 23:55:07 -0700

    Update RAGFileSearch.py

--- RAGFileSearch.py
+++ RAGFileSearch.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import openai
 from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
