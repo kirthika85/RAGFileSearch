b3ba83b68f1e53a568e8c0451b59602596aa3900
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-23 06:44:01 -0700

    Update RAGFileSearch.py

--- RAGFileSearch.py
+++ RAGFileSearch.py
@@ -63,7 +63,7 @@ if openai_api_key.startswith('sk-'):
         print(prompt)
         document_chain = create_stuff_documents_chain(llm=model,prompt=prompt)
         # Retrieving the top 1 relevant document from the vector store , We can change k to 2 and get top 2 and so on
-        retriever = vector_store.as_retriever(search_kwargs={"k": 5})
+        retriever = vector_store.as_retriever(search_kwargs={"k": 0})
         retrieval_chain = create_retrieval_chain(retriever, document_chain)
         return retrieval_chain
 
